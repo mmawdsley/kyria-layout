3b644e11c5841e2b6d6dcebce5aa56e48b37a642
Matthew Mawdsley <matthew@mawdsley.me>
2021-06-29 21:06:07 +0100

    Swap logo and status around

--- keymap.c
+++ keymap.c
@@ -205,9 +205,9 @@ static void render_status(void) {
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
-        render_kyria_logo();
-    } else {
         render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+    } else {
+        render_kyria_logo();
     }
 }
 #endif
