62d2327d64e82d7bd34a7ed80bafd777fc95d54d
Matthew Mawdsley <matthew@mawdsley.me>
2021-06-30 14:29:03 +0100

    Space out status screen

--- keymap.c
+++ keymap.c
@@ -276,6 +276,7 @@ static void render_status(void) {
 
     // QMK Logo and version information
     render_qmk_logo();
+    oled_write_P(PSTR("\n"), false);
     oled_write_P(PSTR("Kyria rev1.0\n\n"), false);
 
     // Host Keyboard Layer Status
