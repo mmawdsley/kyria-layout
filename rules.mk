f897d9af27e1a8abb91c4a931d21d11385d3c251
Matthew Mawdsley <matthew@mawdsley.me>
2021-06-30 10:00:43 +0100

    Revert "Add bongocat"
    This reverts commit 38bdd814bd50d6dcc0f16f01dbb57fef254af75a.

--- rules.mk
+++ rules.mk
@@ -3,4 +3,3 @@ ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
-WPM_ENABLE = yes
