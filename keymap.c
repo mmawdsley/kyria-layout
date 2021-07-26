fa62be9c240ff753f06cf18d49f3cb72407a9ef5
Matthew Mawdsley <matthew@mawdsley.me>
2021-07-26 21:12:25 +0100

    Remove scroll wheel from right encoder

--- keymap.c
+++ keymap.c
@@ -323,23 +323,11 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     }
     else if (index == 1) {
-        switch(biton32(layer_state)){
-        case _LOWER:
-            // Page up/Page down
-            if (clockwise) {
-                tap_code16(LGUI(KC_PGDN));
-            } else {
-                tap_code16(LGUI(KC_PGUP));
-            }
-            break;
-        default:
-            // Scroll up/down
-            if (clockwise) {
-                tap_code(KC_WH_D);
-            } else {
-                tap_code(KC_WH_U);
-            }
-            break;
+        // Page up/Page down
+        if (clockwise) {
+            tap_code16(LGUI(KC_PGDN));
+        } else {
+            tap_code16(LGUI(KC_PGUP));
         }
     }
     return true;
