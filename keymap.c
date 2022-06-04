a5a7b0fd4702582728fb295c2c079ee2b3435b9e
Matthew Mawdsley <matthew@mawdsley.me>
2022-06-04 11:12:11 +0100

    Add right control mod-tap

--- keymap.c
+++ keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bspc  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | Del  | LGUI |  | RGUI | | \  |   N  |   M  | ,  < | . >  | /  ? | RShift |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
       KC_TAB,               KC_Q, KC_W, KC_E,    KC_R,     KC_T,                                              KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-      MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D,    KC_F,     KC_G,                                              KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D,    KC_F,     KC_G,                                              KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_QUOT),
       KC_LSFT,              KC_Z, KC_X, KC_C,    KC_V,     KC_B,    KC_DEL,     KC_LGUI, KC_RGUI, KC_BSLS,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                         KC_MUTE, KC_LCTRL, KC_LALT, MO(_LOWER), KC_ENT,  KC_SPC,  MO(_RAISE), KC_RALT, KC_RCTRL, XXXXXXX
     ),
